e367245a6bd1fc369206c1f84772b70ff36392a9
김정민 <raon37910@gmail.com>
2025-03-29 22:53:21 +0900

    test 글

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -9,10 +9,10 @@ const Comments = () => {
   return (
     // https://giscus.app/ko 를 확인해 주세요
     <Giscus
-      repo="수정해/주세요"
-      repoId="수정해주세요"
-      category="수정해주세요"
-      categoryId="수정해주세요"
+      repo="Juungmini0601/my-simple-blog"
+      repoId="R_kgDOOQuY_g"
+      category="Announcements"
+      categoryId="DIC_kwDOOQuY_s4Cok9A"
       id="comments"
       mapping="pathname"
       reactionsEnabled="1"
--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,16 +1,17 @@
 export const USER_INFORMATIONS = {
-  headerTitle: "EXAMPLE",
-  userName: "EXAMPLE",
-  mascot: "👀",
-  resume: "https://www.EXAMPLE.com",
-  github: "https://www.EXAMPLE.com",
-  domain: "https://www.EXAMPLE.com",
-  meta: {
-    title: "EXAMPLE",
-    description: "EXAMPLE",
-  },
+  "headerTitle": "Jungmini",
+  "userName": "김정민",
+  "mascot": "🐻",
+  "resume": "",
+  "github": "https://github.com/Juungmini0601",
+  "domain": "https://naver.com",
+  "meta": {
+    "title": "Jungmini 개발 블로그",
+    "description": "Jungmini 개발 블로그입니다"
+  }
 };
 
 export const CATEGORY_ICONS: Record<string, string> = {
+  Tech: "🖥️",
   All: "🏠",
 };
